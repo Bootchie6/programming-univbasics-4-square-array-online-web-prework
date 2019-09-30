c4f87e7a432e2e414896371aead1320265e41261
Jonathan Jopio <jonathanjopio@gmail.com>
2019-09-30 04:47:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   counter = 0
-  square[]
+  squared = []
   while array[counter] do
-    square << array[counter] ** 2
+    squared << array[counter]**2
     counter += 1
   end
   return square
