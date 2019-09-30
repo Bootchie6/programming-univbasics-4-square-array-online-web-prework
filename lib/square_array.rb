acfa37f15820f4799e895d7a4f6d7bcceb98d0e7
Jonathan Jopio <jonathanjopio@gmail.com>
2019-09-30 04:42:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,8 @@
 def square_array(array)
   counter = 0
   
-  while counter < array.length do
-    num = array.shift
-    square = num ** 2
-    array.unshift
-    
+  while array[counter] do
+  
     counter += 1
   end
 end
